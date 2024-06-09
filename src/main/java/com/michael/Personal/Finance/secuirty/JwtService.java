111f39d2057e97e1863ecd766e3ddc63c1f0aaed
Johnnymic <johnny25580@gmail.com>
2024-06-09 16:15:49 +0100

    JwtService building tokens

--- src/main/java/com/michael/Personal/Finance/secuirty/JwtService.java
+++ src/main/java/com/michael/Personal/Finance/secuirty/JwtService.java
@@ -6,20 +6,24 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.apache.tomcat.util.buf.UDecoder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
     @Value("${application.security.jwt.secret-key}")
     private   String  secretKey;
     @Value("${application.security.jwt.expiration}")
-    private String expiration;
+    private Long expiration;
     public String extractUsername(String token) {
         return extractClaim(token,Claims::getSubject);
     }
@@ -28,6 +32,7 @@ public class JwtService {
        Claims claims = extractAllClaims(token);
        return claimsResolver.apply(claims);
     }
+
 
     private Claims extractAllClaims(String token) {
       return Jwts
@@ -58,5 +63,38 @@ public class JwtService {
 
     public Date extractExpiration(String token){
        return  extractClaim(token, Claims::getExpiration);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extractClaims,
+            UserDetails userDetails
+    ){
+        return buildToken(extractClaims, userDetails, expiration);
+    }
+
+    private String buildToken(
+            Map<String, Object> extractClaims,
+            UserDetails userDetails,
+            Long expiration) {
+
+        var  authorities = userDetails.
+                getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
+
+        return Jwts.builder()
+                .setClaims(extractClaims)
+                .setClaims(extractClaims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .claim("authorities", authorities)
+                .signWith(getSigningKey())
+                .compact();
+
     }
 }
