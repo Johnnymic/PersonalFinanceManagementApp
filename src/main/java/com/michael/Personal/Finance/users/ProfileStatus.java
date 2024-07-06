5fe518748ac16d97494cee64d4df2df8c39ff909
Johnnymic <johnny25580@gmail.com>
2024-07-06 09:05:51 +0100

    day10

--- src/main/java/com/michael/Personal/Finance/users/ProfileStatus.java
+++ src/main/java/com/michael/Personal/Finance/users/ProfileStatus.java
@@ -1,2 +1,12 @@
-package com.michael.Personal.Finance.users;public enum UserStatus {
+package com.michael.Personal.Finance.users;
+
+public enum ProfileStatus {
+    NEW,
+    ACTIVE,
+    INACTIVE,
+    DISABLED,
+    SUSPENDED,
+    REJECTED,
+    PENDING_ACTIVATION,
+    LOCKED,
 }
